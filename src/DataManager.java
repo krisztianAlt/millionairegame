dee5440d680b73aafd6eb828a3e3fd136f5da56e
Peter Juhasz <jpeter82@gmail.com>
2017-08-15 14:31:37 +0200

    Implement readFromFile method

--- src/DataManager.java
+++ src/DataManager.java
@@ -38,8 +38,26 @@ public class DataManager {
         return maxLineNumbers;
     }
 
-//    public static function readFromFile(filename, line_number) {
-//
-//    }
+    public static String[] readFromFile(int level, int lineNumber) {
+
+        String[] splittedLine = {};
+        try {
+            File questionFile = new File( filePath + fileName + level + fileExtension);
+            Scanner fileContent = new Scanner(questionFile);
+
+            int lineCounter = 0;
+            while (fileContent.hasNextLine()) {
+                String line = fileContent.nextLine();
+                if (lineCounter == lineNumber - 1) {
+                    splittedLine = line.split(";");
+                }
+                lineCounter++;
+            }
+        }
+        catch (FileNotFoundException error) {
+            System.out.println("File not found: " + error);
+        }
+        return splittedLine;
+    }
 
 }
